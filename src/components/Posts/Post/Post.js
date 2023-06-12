27da13dc7b9ad851ab8c406b5c4b9b2f5a24ed6f
fery <67243217+ferytell@users.noreply.github.com>
2023-06-12 12:00:41 +0700

    first commit

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 import useStyles from "./styles";
 
-const Post = ({ post }) => {
+const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
     return (
         <Card className={classes.card}>
@@ -16,8 +16,11 @@ const Post = ({ post }) => {
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}>
-                <button style={{color: 'white'}} size="small" onClick={()=>{}}>
-                    <MoreHorizIcon fontSize="default" />
+                <button 
+                    style={{color: 'white'}} 
+                    size="small" 
+                    onClick={()=> setCurrentId(post._id)}>
+                    <MoreHorizIcon fontSize="medium" />
                 </button>
             </div>
             <div>
